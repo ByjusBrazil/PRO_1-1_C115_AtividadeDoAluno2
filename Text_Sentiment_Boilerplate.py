9acd10a25ddec90c45fe1ced0cde605c7a3b47f2
leonardoaranhabyjus <leonardo.aranha@byjusfutureschool.com>
2022-05-26 11:46:24 -0300

    init

---
+++ Text_Sentiment_Boilerplate.py
@@ -0,0 +1,20 @@
+import tensorflow.keras
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentence = ["Estou feliz em encontrar meus amigos. Estamos planejando ir a uma festa.", 
+            "Eu tive um dia ruim na escola. Eu me machuquei jogando futebol."]
+
+# Tokenização
+
+#Crie um dicionário chamado word_index
+
+# Preenchendo a sequência
+
+# Defina o modelo usando um arquivo .h5
+
+# Teste o modelo
+
+# Imprima o resultado
+
